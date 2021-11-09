526a87eb1eb49873b41d098d8de69eb667dfd703
beytullahsari14 <beytullahsari14@yandex.com>
2021-11-09 16:29:44 +0300

    Initial commit

---
+++ src/ogrenciBılgılerı/Main.java
@@ -0,0 +1,13 @@
+package ogrenciBýlgýlerý;
+
+public class  Main {             
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // sysout ctrl space
+		System.out.println("Öðrenci Bilgileri");
+		System.out.println("Beytullah");
+	    System.out.println("Sarý");
+        System.out.println("Futbol Oynamayý Sever");
+        System.out.println("yaþ : +16");
+        System.out.println("izzet baysal");
